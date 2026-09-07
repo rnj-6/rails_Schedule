d6003a43804ccd4862d835eae2eb1f0df7f9edfb
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-07 23:08:51 +0900

    エラーメッセージを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,17 @@ class Post < ApplicationRecord
       presence: true
 
   validate :dates_are_valid
+
+  validates :body, length: { maximum: 500 }
+
+  private
+
   def dates_are_valid
-    if start_date.present? && end_date.present? && start_date > end_date
+    return if start_date.blank? || end_date.blank?
+    if end_date < start_date
       errors.add(:end_date, "は開始日以降の日付を選択してください")
     end
   end
 
-  validates :body, length: { maximum: 500 }
+  
 end
